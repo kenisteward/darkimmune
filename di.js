8021a272334bb99ae6ae8227da906579000ef899
kenieintern <keni@eintern.com>
2015-07-10 19:21:00 -0400

    initial commit

---
+++ di.js
@@ -0,0 +1,21 @@
+/// <reference path="typings/node/node.d.ts"/>
+var ex = require('express');
+var path = require('path')
+var app = ex();
+
+//setup using jade view engine
+app.set('views', path.join(__dirname, 'templates/jade'));
+app.set('view engine', 'jade');
+
+//setup stylus middleware
+app.use(require('stylus').middleware({src: __dirname + '/templates'
+	, dest: __dirname + '/public'}));
+
+//setup static file responses
+app.use(ex.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res){
+	res.render('home', {"message": "HELLO WORLD!"});
+});
+
+app.listen(process.argv[2] || 53130 );
