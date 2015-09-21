07551f814caa514701c22aa60f14f5243b2e8832
kenieintern <keni@eintern.com>
2015-09-21 11:46:18 -0400

    crazy change

--- public/scripts/site.js
+++
@@ -1,8 +0,0 @@
-/// <reference path="../../typings/jquery/jquery.d.ts"/>
-$().ready(function () {
-	$(".nav a").on("click", function () {
-		$(".nav").find(".active").removeClass("active");
-		$(this).parent().addClass("active");
-	});
-});
-
